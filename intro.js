7735bee15f03eed6eefff87e2c716b2fef6ff3f8
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2020-08-01 19:46:54 +0100

    add a lot lines of code

--- intro.js
+++ intro.js
@@ -1,1 +1,83 @@
+// this is a single-line comment
 console.log('Hello World!')
+console.log(100)
+console.log(0)
+/* this is a 
+* multi-line 
+* comment
+*/
+// Opening line
+console.log('It was love at first sight.');
+/*
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+*/
+console.log('JavaScript')
+console.log(2011)
+console.log('Woohoo! I love to code! #codecademy')
+console.log(20.49)
+
+/*Data Types
+
+Data types are the classifications we give to the different kinds of data that we use in programming.
+ In JavaScript, there are seven fundamental data types:
+
+1 - Number: Any number, including numbers with decimals: 4, 8, 1516, 23.42.
+
+2 - String: Any grouping of characters on your keyboard (letters, numbers, spaces, symbols, etc.)
+surrounded by single quotes: ' ... ' or double quotes " ... ".
+Though we prefer single quotes. Some people like to think of string as a fancy word for text.
+
+3 - Boolean: This data type only has two possible values— either true or false (without quotes).
+It’s helpful to think of booleans as on and off switches or as the answers to a “yes” or “no” question.
+
+4 - Null: This data type represents the intentional absence of a value, and is represented by the keyword null (without quotes).
+
+5 - Undefined: This data type is denoted by the keyword undefined (without quotes).
+It also represents the absence of a value though it has a different use than null.
+
+6 - Symbol: A newer feature to the language, symbols are unique identifiers, useful in more complex coding.
+No need to worry about these for now.
+
+7 - Object: Collections of related data.
+The first 6 of those types are considered primitive data types.
+They are the most basic data types in the language.
+Objects are more complex, and you’ll learn much more about them as you progress through JavaScript.
+At first, seven types may not seem like that many,
+but soon you’ll observe the world opens with possibilities once you start leveraging each one.
+As you learn more about objects, you’ll be able to create complex collections of data.
+
+But before we do that, let’s get comfortable with strings and numbers!
+*/
+
+// Arithmetic Operators
+console.log(32+3.5)
+console.log(2020-1969)
+console.log(65/240)
+console.log(0.2708*100)
+
+// String Concatenation
+console.log("Hello"+"World")
+console.log("Hello"+" "+"World")
+
+// Properties
+console.log("Teaching the world how to code".length)
+
+// Methods
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+// Built-in Objects
+
+console.log(Math.floor(Math.random()*100))
+console.log(Math.ceil(43.8))
+console.log(Number.isInteger(2017))
+
