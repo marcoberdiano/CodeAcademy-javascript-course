9ee0bd235b0be186e38c3d634334e2dbfa4d29c1
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2020-08-01 19:56:07 +0100

    create file objects

--- objects.js
+++ objects.js
@@ -13,7 +13,11 @@ but that deceptively simple task is extremely powerful in practice.
 You’ve been using the power of objects all along, but now it’s time to understand the mechanics of objects and start making your own!
 */
 
-// Creating Object Literals
+/*Creating Object Literals
+Objects can be assigned to variables just like any JavaScript type. We use curly braces, {}, to designate an object literal:
+We fill an object with unordered data. This data is organized into key-value pairs.
+A key is like a variable name that points to a location in memory that holds a value.
+*/
 
 // Write your fasterShip object literal below
 let fasterShip = {
