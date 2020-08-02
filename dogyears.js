4c2a673323dea6514a83a115df2f5b6761cde16f
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2020-08-02 13:28:57 +0100

    create new project

---
+++ dogyears.js
@@ -0,0 +1,24 @@
+// create myage variable
+const myAge = 32
+
+//  create earlyYears variable
+let earlyYears = 2
+earlyYears *= 10.5
+
+// laterYears formulae
+let laterYears = myAge - 2
+
+// dog years 4 years later
+laterYears *= 4
+
+console.log(earlyYears)
+console.log(laterYears)
+
+// calculate my age in dogs years
+const myAgeInDogYears = earlyYears + laterYears
+
+// my name convert to lower case
+const myName = "Marco".toLowerCase()
+
+// log the resuls to the console
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
