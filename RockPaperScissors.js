a0d97e531a04111e8ab7176c230ca767b2e9a6f1
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2020-08-02 17:12:08 +0100

    new lines of code

---
+++ RockPaperScissors.js
@@ -0,0 +1,76 @@
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput==='rock' || userInput==='paper' || userInput==='scissors') {
+      return userInput;
+    } else if (userInput==='bomb'){
+      return userInput;
+    } else {
+      return 'Invalid choice!';
+    }
+  }
+  
+  // test user choice
+  // console.log("User Choice:",getUserChoice('paper'));
+  
+  const getComputerChoice = () => {
+    randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+      case 0:
+        return 'rock';
+        break;
+      case 1:
+        return 'paper';
+        break;
+      case 2:
+        return 'scissors';
+        break;
+    }
+  }
+  // test computer choice
+  // console.log("Computer Choice:",getComputerChoice());
+  
+  function determineWinner(userChoice, computerChoice){
+  
+    if(userChoice===computerChoice){
+      return 'Tie';
+    } else {
+  
+      if (userChoice==='bomb') {
+          return 'User Won!';
+      }
+  
+      if (userChoice === 'rock') {
+        if(computerChoice==='paper'){
+          return 'Computer Won!';
+        } else {
+          return 'User Won!';
+        }
+      }
+  
+      if (userChoice === 'paper') {
+        if(computerChoice==='rock'){
+          return 'User Won!';
+        } else {
+          return 'Computer Won!';
+        }
+      }
+  
+      if (userChoice === 'scissors') {
+        if(computerChoice==='paper'){
+          return 'User Won!';
+        } else {
+          return 'Computer Won!';
+        }
+      }
+    }
+  }
+  
+  function playGame(){
+    userChoice = getUserChoice('bomb');
+    computerChoice = getComputerChoice();
+    console.log(`User Choice: ${userChoice}`)
+    console.log(`Computer Choice: ${computerChoice}`)
+    console.log(determineWinner(userChoice, computerChoice));
+  }
+  
+  playGame();
