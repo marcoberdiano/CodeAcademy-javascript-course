e9c752c188d6bffcc0a45443f68e99f0fbd907fe
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2020-08-02 14:48:32 +0100

    new project

--- magiceightball.js
+++ magiceightball.js
@@ -36,4 +36,24 @@ switch (randomNumber) {
     break;
 }
 
+/*
+if(randomNumber===0) {
+  eigthBall = 'It is certain';
+} else if(randomNumber===1) {
+  eigthBall = 'It is decidedly so';
+} else if(randomNumber===2){
+  eigthBall = 'Reply hazy try again';
+} else if(randomNumber===3){
+  eigthBall = 'Cannot predict now';
+} else if(randomNumber===4){
+  eigthBall = 'Do not count on it';
+} else if(randomNumber===5){
+  eigthBall = 'My sources say no';
+} else if(randomNumber===6){
+  eigthBall = 'Outlook not so good';
+} else {
+  eigthBall = 'Signs point to yes';
+}
+*/
+
 console.log(eigthBall)
