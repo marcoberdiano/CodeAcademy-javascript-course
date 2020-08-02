655dd4ac73cbb32e0cdba25a13aab37a9db22ea6
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2020-08-02 12:43:20 +0100

    create a new file

---
+++ kelvinweather.js
@@ -0,0 +1,13 @@
+// create kelvin variable
+const kelvin = 0;
+
+// celsius formulae
+const celsius = kelvin - 273
+
+// fahrenheit formulae
+let fahrenheit = celsius * (9/5) + 32
+
+// round fahrenheit
+fahrenheit = Math.floor(fahrenheit)
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
