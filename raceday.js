133ad23b9a9e84dd929cde9c72e8d0cd5ede6afd
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2020-08-02 15:16:22 +0100

    new project

--- raceday.js
+++ raceday.js
@@ -2,18 +2,21 @@ let raceNumber = Math.floor(Math.random() * 1000);
 
 let registeredEarly = true;
 
-let runnerAge = 18;
+let runnerAge = 13;
 
 if (runnerAge > 18 && registeredEarly) {
   raceNumber += 1000;
 } 
 
 if (runnerAge > 18 && registeredEarly) {
-  console.log(`${raceNumber}, Early adults run at 9:30 am`)
+  console.log(`Race Number: ${raceNumber}
+  Early adults run at 9:30 am`)
 } else if (runnerAge > 18 && !registeredEarly) {
-  console.log(`${raceNumber}, Late adults run at 11:00 am`)
+  console.log(`Race Number: ${raceNumber}
+  Late adults run at 11:00 am`)
 } else if (runnerAge < 18) {
-  console.log(`${raceNumber}, Youth registrants run at 12:30 pm`)
+  console.log(`Race Number: ${raceNumber}
+  Youth registrants run at 12:30 pm`)
 } else {
   console.log(' Please, see the registration desk')
 }
