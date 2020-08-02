c3fc9aeee98882007b3c960d9b2cbb35cfcb8d56
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2020-08-02 18:44:56 +0100

    new changes

--- SleepDebtCalculator.js
+++ SleepDebtCalculator.js
@@ -1,7 +1,7 @@
 const getSleepHours = day => {
     switch(day) {
       case 'monday':
-        return 4;
+        return 5;
         break;
       case 'tuesday':
         return 8;
@@ -10,32 +10,47 @@ const getSleepHours = day => {
         return 6;
         break;
       case 'thursday':
-        return 3;
-        break;
-      case 'friday':
-        return 5;
-        break;
-      case 'saturday':
         return 7;
         break;
+      case 'friday':
+        return 8;
+        break;
+      case 'saturday':
+        return 3;
+        break;
       case 'sunday':
-        return 2;
+        return 9;
         break;
     }
   }
-  
-const getActualSleepHours = () => {
-    const dayOfTheWeek = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'];
-    
-    let totalSleepHours = 0;
-    
-    dayOfTheWeek.forEach(day => totalSleepHours += getSleepHours(day));
-    
-    return totalSleepHours;
-}
-  
 
-const getIdealSleepHours = () => {
-    const idealHours = 6;
-    return idealHours * 7;
-}
+  const getActualSleepHours = () => {
+    const dayOfTheWeek = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'];
+    let totalSleepHours = 0;
+    dayOfTheWeek.forEach(day => totalSleepHours += getSleepHours(day));
+    return totalSleepHours;
+  }
+
+  const getIdealSleepHours = idealHours => idealHours * 7;
+
+  const calculateSleepDebt = () => {
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = getIdealSleepHours(8);
+    if (actualSleepHours===idealSleepHours) {
+      console.log('Total ideal hours:',idealSleepHours);
+      console.log('Total actual sleep hours:',actualSleepHours);
+      console.log('Diference:',actualSleepHours-idealSleepHours);
+      console.log('Perfect amount of sleep.');
+    } else if (actualSleepHours>idealSleepHours) {
+      console.log('Total ideal hours:',idealSleepHours);
+      console.log('Total actual sleep hours:',actualSleepHours);
+      console.log('Diference:',actualSleepHours-idealSleepHours);
+      console.log('Got more sleep than needed.');
+    } else {
+      console.log('Total ideal hours:',idealSleepHours);
+      console.log('Total actual sleep hours:',actualSleepHours);
+      console.log('Diference:',actualSleepHours-idealSleepHours);
+      console.log('Should get some rest.');
+    }
+  }
+  calculateSleepDebt();
