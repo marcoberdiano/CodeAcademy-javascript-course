791ddb5b6455844698a686baea25a04f0ebb1752
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2020-08-03 17:42:04 +0100

    finished secret message

---
+++ SecretMessage.js
@@ -0,0 +1,24 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+console.log(secretMessage.length)
+secretMessage.pop()
+console.log(secretMessage.length)
+
+secretMessage.push("to","Program")
+
+secretMessage[secretMessage.indexOf('easily')] = 'right'
+console.log(secretMessage)
+
+secretMessage.shift()
+console.log(secretMessage)
+
+secretMessage.unshift('Programming')
+console.log(secretMessage)
+
+secretMessage.forEach(element => {
+    if(element==='get' || element ==='right' || element==='the' || element==='first' || element==='time,') {
+      secretMessage[secretMessage.indexOf(element)] = 'know';
+    }
+  });
+
+  console.log(secretMessage.join(' '))
