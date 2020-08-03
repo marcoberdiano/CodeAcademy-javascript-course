84d44aa8c7ff246b49103334f680f9179e28e145
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2020-08-03 18:48:35 +0100

    finished whaletalk project

---
+++ WhaleTalk.js
@@ -0,0 +1,28 @@
+const input = "turpentine and turtles";
+const input2 = 'Hi, Human'
+const input3 = "turpentine and turtles";
+const input4 = 'a whale of a deal!'
+
+const vowels = ['a','e','i','o','u'];
+
+resultArray = [];
+
+// iterate the input variable
+function whaletakal (input){
+    for(let i = 0; i < input.length; i++) {
+    for(let j = 0; j < vowels.length; j++) {
+        if (input[i]==vowels[j]){
+        resultArray.push(input[i].toUpperCase());
+        }
+    }
+    if(input[i]=='e'){
+        resultArray.push(input[i].toUpperCase());
+    }
+    if(input[i]=='u'){
+        resultArray.push(input[i].toUpperCase());
+    }
+    }
+    console.log(resultArray.join(''));
+}
+
+whaletakal(input4);
