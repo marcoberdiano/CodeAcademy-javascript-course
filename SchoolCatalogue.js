656397caeac2c2143e177bbfa0be354c7019dfae
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2020-08-07 15:33:25 +0100

    new line of code

--- SchoolCatalogue.js
+++ SchoolCatalogue.js
@@ -1,8 +1,10 @@
 class School {
-    constructor(name,level,numberOfStudents){
+    constructor(name,level,numberOfStudents,averageTestScores, schoolOverview){
         this._name = name;
         this._level = level;
         this._numberOfStudents = numberOfStudents;
+        this._averageTestScores = averageTestScores;
+        this._schoolOverview = schoolOverview;
     }
     get name(){
         return this._name;
@@ -13,6 +15,12 @@ class School {
     get numberOfStudents(){
         return this._numberOfStudents;
     }
+    get averageTestScores(){
+        return this._averageTestScores;
+    }
+    get schoolOverview(){
+        return this._schoolOverview;
+    }
     set numberOfStudents(newNumberOfStudents){
         if(Number.isInteger(newNumberOfStudents)){
             this._numberOfStudents = newNumberOfStudents;
@@ -21,14 +29,15 @@ class School {
         }
     }
     quickFacts(){
-        console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`);
+      console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`);
     }
-    static pickSubstituteTeacher(){
-
+    static pickSubstituteTeacher(substituteTeachers){
+      const randomTeacher = Math.floor(Math.random()*substituteTeachers.length);
+      return substituteTeachers[randomTeacher];
     }
 }
 
-class Primary extends School{
+class PrimarySchool extends School{
     constructor(name, level, numberOfStudents, pickUpPolicy){
         super(name,level,numberOfStudents);
         this._pickUpPolicy = pickUpPolicy;
@@ -36,15 +45,18 @@ class Primary extends School{
     get pickUpPolicy(){
         return this._pickUpPolicy;
     }
+    set pickUpPolicy(policy){
+      this._pickUpPolicy = policy;
+    }
 }
 
-class Middle extends School {
+class MiddleSchool extends School {
     constructor(name, level, numberOfStudents){
         super(name,level,numberOfStudents);
     }
 }
 
-class High extends School {
+class HighSchool extends School {
     constructor(name, level, numberOfStudents, sportsTeams){
         super(name,level,numberOfStudents);
         this._sportsTeams = sportsTeams;
@@ -54,5 +66,29 @@ class High extends School {
     }
 }
 
-const school = new School('LDR',4,50);
-school.quickFacts();
+class SchoolCatalog {
+  constructor(schools){
+    this._schools = [schools];
+  }
+  get schools(){
+    return this._schools;
+  }
+  addSchools(school){
+      this._schools.push(school);
+  }
+}
+
+lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 'primary',514,'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+
+lorraineHansbury.quickFacts();
+console.log(School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']));
+
+alSmith = new HighSchool('Al E. Smith','high', 415,['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+
+console.log(alSmith.sportsTeams);
+
+const primarySchools = new SchoolCatalog(lorraineHansbury);
+const middleSchools = new SchoolCatalog();
+const higthSchools = new SchoolCatalog(alSmith);
+
+console.log(primarySchools.schools);
